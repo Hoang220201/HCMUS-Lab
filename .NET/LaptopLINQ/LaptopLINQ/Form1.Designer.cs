fd50a2b3e63aff3941da99e42534f8ff3dcdc80a
hoang220201 <hoang22201@gmail.com>
2022-09-21 17:30:51 +0700

    .NET

---
+++ .NET/LaptopLINQ/LaptopLINQ/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace LaptopLINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwLaptop = new System.Windows.Forms.DataGridView();
+            this.colLaptopID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLaptopName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLaptopType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colProductDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colProcessor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colHDD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRAM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.picLaptop = new System.Windows.Forms.PictureBox();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.btnCloseApp = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwLaptop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLaptop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwLaptop
+            // 
+            this.dgwLaptop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwLaptop.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colLaptopID,
+            this.colLaptopName,
+            this.colLaptopType,
+            this.colProductDate,
+            this.colProcessor,
+            this.colHDD,
+            this.colRAM,
+            this.colPrice});
+            this.dgwLaptop.Location = new System.Drawing.Point(16, 61);
+            this.dgwLaptop.Name = "dgwLaptop";
+            this.dgwLaptop.Size = new System.Drawing.Size(470, 204);
+            this.dgwLaptop.TabIndex = 0;
+            this.dgwLaptop.SelectionChanged += new System.EventHandler(this.dgwLaptop_SelectionChanged);
+            // 
+            // colLaptopID
+            // 
+            this.colLaptopID.DataPropertyName = "LaptopID";
+            this.colLaptopID.HeaderText = "LaptopID";
+            this.colLaptopID.Name = "colLaptopID";
+            // 
+            // colLaptopName
+            // 
+            this.colLaptopName.DataPropertyName = "LaptopName";
+            this.colLaptopName.HeaderText = "LaptopName";
+            this.colLaptopName.Name = "colLaptopName";
+            // 
+            // colLaptopType
+            // 
+            this.colLaptopType.DataPropertyName = "LaptopType";
+            this.colLaptopType.HeaderText = "LaptopType";
+            this.colLaptopType.Name = "colLaptopType";
+            // 
+            // colProductDate
+            // 
+            this.colProductDate.DataPropertyName = "ProductDate";
+            this.colProductDate.HeaderText = "ProductDate";
+            this.colProductDate.Name = "colProductDate";
+            // 
+            // colProcessor
+            // 
+            this.colProcessor.DataPropertyName = "Processor";
+            this.colProcessor.HeaderText = "Processor";
+            this.colProcessor.Name = "colProcessor";
+            // 
+            // colHDD
+            // 
+            this.colHDD.DataPropertyName = "HDD";
+            this.colHDD.HeaderText = "HDD";
+            this.colHDD.Name = "colHDD";
+            // 
+            // colRAM
+            // 
+            this.colRAM.DataPropertyName = "RAM";
+            this.colRAM.HeaderText = "RAM";
+            this.colRAM.Name = "colRAM";
+            // 
+            // colPrice
+            // 
+            this.colPrice.DataPropertyName = "Price";
+            this.colPrice.HeaderText = "Price";
+            this.colPrice.Name = "colPrice";
+            // 
+            // picLaptop
+            // 
+            this.picLaptop.Location = new System.Drawing.Point(516, 61);
+            this.picLaptop.Name = "picLaptop";
+            this.picLaptop.Size = new System.Drawing.Size(216, 204);
+            this.picLaptop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLaptop.TabIndex = 1;
+            this.picLaptop.TabStop = false;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(16, 33);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(203, 22);
+            this.btnLoadData.TabIndex = 2;
+            this.btnLoadData.Text = "Load Data From SQL Server";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // btnCloseApp
+            // 
+            this.btnCloseApp.Location = new System.Drawing.Point(268, 33);
+            this.btnCloseApp.Name = "btnCloseApp";
+            this.btnCloseApp.Size = new System.Drawing.Size(171, 22);
+            this.btnCloseApp.TabIndex = 3;
+            this.btnCloseApp.Text = "Close Application";
+            this.btnCloseApp.UseVisualStyleBackColor = true;
+            this.btnCloseApp.Click += new System.EventHandler(this.btnCloseApp_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(268, 271);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 28);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(163, 271);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 28);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(46, 271);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 28);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnCloseApp);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.picLaptop);
+            this.Controls.Add(this.dgwLaptop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwLaptop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLaptop)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwLaptop;
+        private System.Windows.Forms.PictureBox picLaptop;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Button btnCloseApp;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLaptopID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLaptopName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLaptopType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colProductDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colProcessor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colHDD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRAM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
+    }
+}
+
---
+++ .NET/LaptopLINQ/LaptopLINQ/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LaptopLINQ
+{
+    public partial class Form1 : Form
+    {
+        static string ProjectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        LaptopDBDataContext db = new LaptopDBDataContext();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picLaptop.Image = Image.FromFile(ProjectPath + "\\Data\\laptop.jpg");
+            picLaptop.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btnCloseApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void DisplayData()
+        {
+            dgwLaptop.DataSource = (from sp in db.Laptops select sp);
+            dgwLaptop.Columns["ImageName"].Visible = false;
+            dgwLaptop.Columns["colLaptopID"].ReadOnly = true;
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private void dgwLaptop_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgwLaptop.Focused == false || dgwLaptop.CurrentCell == null)
+                return;
+            int RowIndex = dgwLaptop.CurrentRow.Index;
+            string SelectedLaptopID = dgwLaptop.Rows[RowIndex].Cells[0].Value.ToString();
+
+            string LaptopImage = (from sp in db.Laptops
+                                  where sp.LaptopID == SelectedLaptopID
+                                  select sp.ImageName).FirstOrDefault();
+            if (LaptopImage == null)
+                return;
+            picLaptop.Image = Image.FromFile(ProjectPath + "\\Data\\" + LaptopImage);
+            picLaptop.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int RowIndex = dgwLaptop.CurrentRow.Index;
+            string SelectedLaptopID = dgwLaptop.Rows[RowIndex].Cells[0].Value.ToString();
+
+            Laptop deletedSP = (from sp in db.Laptops
+                                where sp.LaptopID == SelectedLaptopID
+                                select sp).SingleOrDefault();
+            db.Laptops.DeleteOnSubmit(deletedSP);
+            db.SubmitChanges();
+            DisplayData();
+
+            MessageBox.Show("Laptop :" + deletedSP.LaptopID + "is deleted!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int RowIndex = dgwLaptop.CurrentRow.Index;
+            string SelectedLaptopID = dgwLaptop.Rows[RowIndex].Cells[0].Value.ToString();
+
+            Laptop updateSP = (from sp in db.Laptops
+                               where sp.LaptopID == SelectedLaptopID
+                               select sp).SingleOrDefault();
+
+            updateSP.LaptopID = dgwLaptop.Rows[RowIndex].Cells[0].Value.ToString();
+            updateSP.LaptopName = dgwLaptop.Rows[RowIndex].Cells[1].Value.ToString();
+            updateSP.LaptopType = dgwLaptop.Rows[RowIndex].Cells[2].Value.ToString();
+            updateSP.ProductDate = DateTime.Parse(dgwLaptop.Rows[RowIndex].Cells[3].Value.ToString());
+            updateSP.Processor = dgwLaptop.Rows[RowIndex].Cells[4].Value.ToString();
+            updateSP.HDD = dgwLaptop.Rows[RowIndex].Cells[5].Value.ToString();
+            updateSP.RAM = dgwLaptop.Rows[RowIndex].Cells[6].Value.ToString();
+            updateSP.Price = Convert.ToInt32(dgwLaptop.Rows[RowIndex].Cells[7].Value.ToString());
+            updateSP.ImageName = "laptop.jpg";
+
+            db.SubmitChanges();
+            DisplayData();
+            MessageBox.Show("Laptop :" + updateSP.LaptopID + " is updated");
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            Laptop checkedSP;
+            List<Laptop> SPList = (from sp in db.Laptops
+                                   where sp.LaptopID.Contains("NEW")
+                                   select sp).ToList();
+            string NewID = "NEW";
+            if (SPList.Count == 0)
+                NewID = NewID + "001";
+            else
+            {
+                checkedSP = SPList.Last();
+                NewID = NewID + NextId(checkedSP.LaptopID.Substring(checkedSP.LaptopID.Length - 3));
+
+            }
+
+            Laptop insertedSP = new Laptop();
+            insertedSP.LaptopID = NewID;
+            insertedSP.LaptopName = "Not Assigned";
+            insertedSP.LaptopType = "Not Assigned";
+            insertedSP.ProductDate = DateTime.Now;
+            insertedSP.Processor = "Not Assigned";
+            insertedSP.HDD = "Not Assigned";
+            insertedSP.RAM = "Not Assigned";
+            insertedSP.Price = 0;
+            insertedSP.ImageName = "laptop.jpg";
+
+            db.Laptops.InsertOnSubmit(insertedSP);
+            db.SubmitChanges();
+            DisplayData();
+            MessageBox.Show("New laptop is inserted! Please update data and press button Update");
+        }
+        public string NextId(string currentId)
+        {
+            int i = 0;
+            if (int.TryParse(currentId, out i))
+            {
+                i++;
+                return (i.ToString().PadLeft(3, '0'));
+            }
+            throw new System.ArgumentException("Non-numeric string passed as argument");
+        }
+    }
+}
